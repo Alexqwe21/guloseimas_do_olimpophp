3f2d043c5fb05e6ddce785cbfcef49e6f262cf58
alex sandro <alexsandropimenta16@gmail.com>
2025-01-22 08:15:45 -0300

    alex

--- app/views/dash/dashboard.php
+++ app/views/dash/dashboard.php
@@ -134,7 +134,7 @@
             <div class="sidebar-wrapper">
                 <nav class="mt-2"> <!--begin::Sidebar Menu-->
                     <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
-                        <li class="nav-item menu-open"> <a href="#" class="nav-link active"> <i class="bi bi-speedometer2"></i>
+                        <li class="nav-item menu-open"> <a href="http://localhost/guloseimas_do_olimpophp/public/dashboard" class="nav-link active"> <i class="bi bi-speedometer2"></i>
                                 <p>
                                     Dashboard
 
